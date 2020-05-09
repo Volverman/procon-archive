e0e07f7da720f1813d1138a19a9e3d2800d75572
Volverman <volverman1017@gmail.com>
2020-05-10 08:09:42 +0900

    [AC] abc149 abc149_c

---
+++ atcoder.jp/abc149/abc149_c/Main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0; i < n; i++)
+#define REPR(i, n) for(int i = n; i >= 0; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define INF 2e9
+#define ALL(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+
+bool IsPrime(int num)
+{
+    if (num < 2) return false;
+    else if (num == 2) return true;
+    else if (num % 2 == 0) return false; // 偶数はあらかじめ除く
+
+    double sqrtNum = sqrt(num);
+    for (int i = 3; i <= sqrtNum; i += 2)
+    {
+        if (num % i == 0)
+        {
+            // 素数ではない
+            return false;
+        }
+    }
+
+    // 素数である
+    return true;
+}
+
+
+int main()
+{
+  int x;
+  cin >> x;
+
+  while(!IsPrime(x)){
+    x++;
+  }
+  cout << x << endl;
+}
