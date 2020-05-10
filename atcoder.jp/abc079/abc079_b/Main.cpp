464611a0fa7a353753fb21b43f28eb2434ca0ef4
Volverman <volverman1017@gmail.com>
2020-05-11 03:33:48 +0900

    [AC] abc079 abc079_b

---
+++ atcoder.jp/abc079/abc079_b/Main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0; i < n; i++)
+#define REPR(i, n) for(int i = n; i >= 0; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define INF 2e9
+#define ALL(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+ 
+int main()
+{
+  ll N;
+  cin >> N;
+
+  ll a = 2;
+  ll b = 1;
+  REP(i,N-1){
+    ll c = a+b;
+    a = b;
+    b = c;
+  }
+  cout << b << endl;
+}
