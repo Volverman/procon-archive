e15ef82ded2152223f39979b633c6e61dc11151c
Volverman <volverman1017@gmail.com>
2020-06-10 23:27:03 +0900

    [AC] abc156 abc156_d

---
+++ atcoder.jp/abc156/abc156_d/Main.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0; i < n; i++)
+#define REPR(i, n) for(int i = n; i >= 0; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define INF 2e9
+#define MOD 1000000007
+#define ALL(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+using P = pair<int,int>;
+
+const int mod = 1000000007;
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const { return pow(mod-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+mint pow_kai(mint a, ll n){//aのn乗を計算します。
+  mint x = 1;
+  while(n > 0){//全てのbitが捨てられるまで。
+    if(n&1){//1番右のbitが1のとき。
+      x = x*a;
+    }
+    a = a*a;
+    n >>= 1;//bit全体を右に1つシフトして一番右を捨てる。
+  }
+  return x;
+}
+ 
+vector<long long> fact_inv, inv, Com;
+ 
+/*  init_nCk :二項係数のための前処理
+    計算量:O(n)
+*/
+void init_nCk(int n, int SIZE) {
+    fact_inv.resize(SIZE + 5);
+    inv.resize(SIZE + 5);
+    fact_inv[0] = fact_inv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < SIZE + 5; i++) {
+        inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
+        fact_inv[i] = fact_inv[i - 1] * inv[i] % MOD;
+    }
+    Com.resize(SIZE + 5);
+    Com[0] = 1;
+    for (int i = 1; i < SIZE + 5; i++) {
+        Com[i] = Com[i - 1] * ((n - i + 1) * inv[i] % MOD) % MOD;
+    }
+}
+ 
+/*  nCk :MODでの二項係数を求める(前処理 int_nCk が必要)
+    計算量:O(1)
+*/
+long long nCk(int k) {
+    assert(!(k < 0));
+    return Com[k];
+}
+int main()
+{
+  ll n,a,b;
+  cin >> n >> a >> b;
+  mint ans = pow_kai(2,n);
+  init_nCk(n,a);
+  ll A = nCk(a);
+  init_nCk(n,b);
+  ll B = nCk(b);
+  mint C = A+B;
+  cout << ans-C-1 << endl;
+}
