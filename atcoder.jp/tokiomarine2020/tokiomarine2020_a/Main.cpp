fdcb38f7d8e4c8ed0d2f2d8aaab43fe1a19583cd
Volverman <volverman1017@gmail.com>
2020-06-13 21:01:06 +0900

    [AC] tokiomarine2020 tokiomarine2020_a

---
+++ atcoder.jp/tokiomarine2020/tokiomarine2020_a/Main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0; i < n; i++)
+#define REPR(i, n) for(int i = n; i >= 0; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define INF 2e9
+#define MOD 1000000007
+#define ALL(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+using P = pair<int,int>;
+
+int main()
+{
+  string S;
+  cin >> S;
+  cout << S.at(0) << S.at(1) << S.at(2) << endl;
+}
