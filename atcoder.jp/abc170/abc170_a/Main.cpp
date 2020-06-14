7957c313e5932c48ea7df85e91545008192b37e7
Volverman <volverman1017@gmail.com>
2020-06-14 21:02:26 +0900

    [AC] abc170 abc170_a

---
+++ atcoder.jp/abc170/abc170_a/Main.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0; i < n; i++)
+#define REPR(i, n) for(int i = n; i >= 0; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define INF 2e9
+#define MOD 1000000007
+#define ALL(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+using P = pair<int,int>;
+
+int main()
+{
+  int a,b,c,d,e;
+  cin >> a >> b >> c >> d >> e;
+  if(a==0){
+    cout << 1 << endl;
+  }
+  if(b==0){
+    cout << 2 << endl;
+  }
+  if(c==0){
+    cout << 3 << endl;
+  }
+  if(d==0){
+    cout << 4 << endl;
+  }
+  if(e==0){
+    cout << 5 << endl;
+  }
+}
