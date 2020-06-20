6cc6310fa0446c1d1d1cdc03108cb77a5b4df2cb
Volverman <volverman1017@gmail.com>
2020-06-20 09:34:58 +0900

    [AC] sumitrust2019 sumitb2019_e

---
+++ atcoder.jp/sumitrust2019/sumitb2019_e/Main.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0; i < n; i++)
+#define REPR(i, n) for(int i = n; i >= 0; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define INF 2e9
+#define MOD 1000000007
+#define ALL(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+using P = pair<int,int>;
+
+const int mod = 1000000007;
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const { return pow(mod-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+int main()
+{
+  ll N;
+  cin >> N;
+  vector<ll> A(N);
+  REP(i, N) cin >> A[i];
+  
+  mint ans = 3;
+  vector<ll> B(3);
+  B.at(0)=1;
+  B.at(1)=0;
+  B.at(2)=0;
+
+  FOR(i,1,N){
+    ll cnt = 0;
+    ll flag=0;
+    REP(j,3){
+      if(A.at(i)==B.at(j)){
+        if(flag==0){
+          B.at(j)++;
+          flag=1;
+        }
+        cnt++;
+      }
+    }
+    ans*=cnt;
+  }
+  if(A[0]!=0){
+    cout << 0 << endl;
+  }else{
+    cout << ans << endl;
+  }
+}
