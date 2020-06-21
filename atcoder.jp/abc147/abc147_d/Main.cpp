4e68c16b56187456df1ac01c8916f724e9e58eb6
Volverman <volverman1017@gmail.com>
2020-06-22 00:49:45 +0900

    [AC] abc147 abc147_d

---
+++ atcoder.jp/abc147/abc147_d/Main.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0; i < n; i++)
+#define REPR(i, n) for(int i = n; i >= 0; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define INF 2e9
+#define MOD 1000000007
+#define ALL(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+using P = pair<int,int>;
+const int mod = 1000000007;
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const { return pow(mod-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+int main()
+{
+  ll N;
+  cin >> N;
+  vector<ll> A(N);
+  REP(i, N) cin >> A[i];
+  
+  int table[N][60];
+  REP(i,N){
+    REP(j,60){
+      table[i][j]=(A[i]>>j)&1;
+    }
+  }
+
+  mint ans = 0;
+  REP(i,60){
+    int cnt = 0;
+    REP(j,N){
+      if(table[j][i]==1){
+        cnt++;
+      }
+    }
+    mint x = cnt*(N-cnt);
+    ans+=x*mint(2).pow(i);
+  }
+
+  cout << ans << endl;
+}
