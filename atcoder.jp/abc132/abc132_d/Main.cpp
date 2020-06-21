860d0ba26b62fcf0c430df441e88578f06d2211d
Volverman <volverman1017@gmail.com>
2020-06-21 12:34:10 +0900

    [AC] abc132 abc132_d

---
+++ atcoder.jp/abc132/abc132_d/Main.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0; i < n; i++)
+#define REPR(i, n) for(int i = n; i >= 0; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define INF 2e9
+#define MOD 1000000007
+#define ALL(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+using P = pair<int,int>;
+
+const int mod = 1000000007;
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const { return pow(mod-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+vector<long long> fact_inv, inv, Com;
+
+void init_nCk(int n, int SIZE) {
+    fact_inv.resize(SIZE + 5);
+    inv.resize(SIZE + 5);
+    fact_inv[0] = fact_inv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < SIZE + 5; i++) {
+        inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
+        fact_inv[i] = fact_inv[i - 1] * inv[i] % MOD;
+    }
+    Com.resize(SIZE + 5);
+    Com[0] = 1;
+    for (int i = 1; i < SIZE + 5; i++) {
+        Com[i] = Com[i - 1] * ((n - i + 1) * inv[i] % MOD) % MOD;
+    }
+}
+ 
+long long nCk(int k) {
+    assert(!(k < 0));
+    return Com[k];
+}
+
+int main()
+{
+  int N,K;
+  cin >> N >> K;
+
+  FOR(i,1,K+1){
+    mint ans = 1;
+    if(i>N-K+1){
+      cout << 0 << endl;
+    }else{
+      init_nCk(K-1,i-1);
+      ans*=nCk(i-1);
+      init_nCk(N-K+1,i);
+      ans*=nCk(i);
+      cout << ans << endl;
+    }
+  }
+}
