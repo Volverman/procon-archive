71278338f6e389a59bf605fdabc055ba10952489
Volverman <volverman1017@gmail.com>
2020-06-26 15:52:46 +0900

    [AC] abc085 abc085_b

---
+++ atcoder.jp/abc085/abc085_b/Main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0; i < n; i++)
+#define REPR(i, n) for(int i = n; i >= 0; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define INF 2e9
+#define MOD 1000000007
+#define ALL(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+using P = pair<int,int>;
+
+int main()
+{
+  int N;
+  cin >> N;
+  vector<int> A(N);
+  set<int> st;
+  REP(i, N){
+    cin >> A[i];
+    st.insert(A[i]);
+  }
+  
+  cout << st.size() << endl;
+}
