703912b084a0cc7bb0d854d50c281a8815e67f2c
Volverman <volverman1017@gmail.com>
2020-07-25 21:01:39 +0900

    [AC] m-solutions2020 m_solutions2020_a

---
+++ atcoder.jp/m-solutions2020/m_solutions2020_a/Main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0; i < n; i++)
+#define REPR(i, n) for(int i = n; i >= 0; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define INF 2e9
+#define MOD 1000000007
+#define ALL(v) v.begin(), v.end()
+#define sz(x) int(x.size())
+using namespace std;
+typedef long long ll;
+using P = pair<int,int>;
+
+int main()
+{
+  int X;
+  cin >> X;
+  cout << 10-X/200 << endl;
+}
