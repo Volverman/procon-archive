2e89a4299e941a33933eac6657fb0041fd59e096
Volverman <volverman1017@gmail.com>
2020-08-25 09:33:31 +0900

    [AC] abc026 abc026_d

---
+++ atcoder.jp/abc026/abc026_d/Main.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+#define REP(i, n) for(int i = 0; i < n; i++)
+#define REP2(i, a, b) for(int i = a;i <= b;i++)
+#define REPR(i, a, b) for(int i = a; i >= b; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define INF 2e9
+#define MOD 1000000007
+#define PI 3.14159265358979323846
+#define vi vector<int>
+#define vll vector<ll>
+#define vi2 vector<vector<int>>
+#define eb emplace_back
+#define fi first
+#define se second
+#define ALL(v) v.begin(), v.end()
+#define sz(x) int(x.size())
+using namespace std;
+using P = pair<ll,ll>;
+const int dx[]{0, 1, 0, -1, -1, -1, 1, 1}, dy[]{1, 0, -1, 0, -1, 1, -1, 1};
+#define INT(name) int name;cin >> name;
+#define VEC(type,name,n) vector<type> name(n);REP(i,n) cin >> name[i];
+
+template<class T> inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+double A,B,C;
+double calc(double x){
+  return A*x+B*sin(C*x*PI)-100;
+}
+
+
+int main()
+{
+  cin >> A >> B >> C;
+  double right=300;
+  double left=0;
+  while(right-left>0.000000000001){
+    double mid=(right+left)/2.0;
+    if(calc(mid)>=0){
+      right=mid;
+    }else{
+      left=mid;
+    }
+  }
+  cout << fixed << setprecision(10) << right << endl;
+  cout << calc(right) << endl;
+}
