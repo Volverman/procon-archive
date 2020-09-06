960021c76791279492ede0e6ad3e0d17e1a12c11
Volverman <volverman1017@gmail.com>
2020-09-07 06:42:46 +0900

    [AC] arc039 arc039_b

---
+++ atcoder.jp/arc039/arc039_b/Main.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+#define REP(i, n) for(int i = 0; i < n; i++)
+#define REP2(i, a, b) for(int i = a;i <= b;i++)
+#define REPR(i, a, b) for(int i = a; i >= b; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define INF 2e9
+#define MOD 1000000007
+#define PI 3.14159265358979323846
+#define vi vector<int>
+#define vll vector<ll>
+#define vi2 vector<vector<int>>
+#define eb emplace_back
+#define fi first
+#define se second
+#define ALL(v) v.begin(), v.end()
+#define sz(x) int(x.size())
+using namespace std;
+using P = pair<ll,ll>;
+const int dx[]{0, 1, 0, -1, -1, -1, 1, 1}, dy[]{1, 0, -1, 0, -1, 1, -1, 1};
+#define INT(name) int name;cin >> name;
+#define VEC(type,name,n) vector<type> name(n);REP(i,n) cin >> name[i];
+
+template<class T> inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+vector<long long> fact_inv, inv, Com;
+
+void init_nCk(int n, int SIZE) {
+    fact_inv.resize(SIZE + 5);
+    inv.resize(SIZE + 5);
+    fact_inv[0] = fact_inv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < SIZE + 5; i++) {
+        inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
+        fact_inv[i] = fact_inv[i - 1] * inv[i] % MOD;
+    }
+    Com.resize(SIZE + 5);
+    Com[0] = 1;
+    for (int i = 1; i < SIZE + 5; i++) {
+        Com[i] = Com[i - 1] * ((n - i + 1) * inv[i] % MOD) % MOD;
+    }
+}
+ 
+long long nCk(int k) {
+    assert(!(k < 0));
+    return Com[k];
+}
+int main()
+{
+  INT(N);
+  INT(K);
+  if(N<=K){
+    init_nCk(N,N);
+    cout << nCk(K%N) << endl;
+  }else{
+    init_nCk(K+N-1,K+N-1);
+    cout << nCk(K) << endl;
+  }
+}
