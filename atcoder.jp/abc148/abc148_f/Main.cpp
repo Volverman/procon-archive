834adc4a70a275eb28cea6d724ee5cc5d926ffe7
Volverman <volverman1017@gmail.com>
2020-09-12 16:48:48 +0900

    [AC] abc148 abc148_f

---
+++ atcoder.jp/abc148/abc148_f/Main.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+#define REP(i, n) for(int i = 0; i < n; i++)
+#define REP2(i, a, b) for(int i = a;i <= b;i++)
+#define REPR(i, a, b) for(int i = a; i >= b; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define INF 2e9
+#define MOD 1000000007
+#define PI 3.14159265358979323846
+#define vi vector<int>
+#define vll vector<ll>
+#define vi2 vector<vector<int>>
+#define eb emplace_back
+#define fi first
+#define se second
+#define ALL(v) v.begin(), v.end()
+#define sz(x) int(x.size())
+using namespace std;
+using P = pair<ll,ll>;
+const int dx[]{0, 1, 0, -1, -1, -1, 1, 1}, dy[]{1, 0, -1, 0, -1, 1, -1, 1};
+#define INT(name) int name;cin >> name;
+#define VEC(type,name,n) vector<type> name(n);REP(i,n) cin >> name[i];
+
+template<class T> inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+struct edge{ll to, cost;};
+typedef pair<ll,ll> P;
+struct graph{
+  ll V;
+  vector<vector<edge> > G;
+  vector<ll> d;
+
+  graph(ll n){
+    init(n);
+  }
+
+  void init(ll n){
+    V = n;
+    G.resize(V);
+    d.resize(V);
+    REP(i,V){
+      d[i] = INF;
+    }
+  }
+
+  void add_edge(ll s, ll t, ll cost){
+    edge e;
+    e.to = t, e.cost = cost;
+    G[s].push_back(e);
+  }
+
+  void dijkstra(ll s){
+    REP(i,V){
+      d[i] = INF;
+    }
+    d[s] = 0;
+    priority_queue<P,vector<P>, greater<P> > que;
+    que.push(P(0,s));
+    while(!que.empty()){
+      P p = que.top(); que.pop();
+      ll v = p.second;
+      if(d[v]<p.first) continue;
+      for(auto e : G[v]){
+        if(d[e.to]>d[v]+e.cost){
+          d[e.to] = d[v]+e.cost;
+          que.push(P(d[e.to],e.to));
+        }
+      }
+    }
+  }
+};
+int main()
+{
+  ll N,t,a;
+  cin >> N >> t >> a;
+  t--;
+  a--;
+  vector<P> A(N-1);
+  REP(i,N-1){
+    cin >> A[i].first >> A[i].second;
+    A[i].fi--;
+    A[i].se--;
+  }
+  graph g(N);
+  REP(i,N-1){
+    g.add_edge(A[i].fi,A[i].se,1);
+    g.add_edge(A[i].se,A[i].fi,1);
+  }
+  g.dijkstra(t);
+  vector<ll> taka=g.d;
+  g.dijkstra(a);
+  vector<ll> aoki=g.d;
+
+  // REP(i,N){
+  //   cout << taka[i] << " " << aoki[i] << endl;
+  // }
+
+  
+
+  ll MAX2=-1;
+  REP(i,N){
+    if(taka[i]<aoki[i]){
+      MAX2=max(MAX2,aoki[i]);
+    }
+  }
+
+  cout << MAX2-1 << endl;
+}
