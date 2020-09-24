e362d45e86413e639d07ab2ef80c457662206e51
Volverman <volverman1017@gmail.com>
2020-09-25 00:52:09 +0900

    [AC] cf16-final codefestival_2016_final_c

---
+++ atcoder.jp/cf16-final/codefestival_2016_final_c/Main.cpp
@@ -0,0 +1,140 @@
+#include <bits/stdc++.h>
+
+#include <algorithm>
+#include <cassert>
+#include <vector>
+
+namespace atcoder {
+
+// Implement (union by size) + (path compression)
+// Reference:
+// Zvi Galil and Giuseppe F. Italiano,
+// Data structures and algorithms for disjoint set union problems
+struct dsu {
+  public:
+    dsu() : _n(0) {}
+    dsu(int n) : _n(n), parent_or_size(n, -1) {}
+
+    int merge(int a, int b) {
+        assert(0 <= a && a < _n);
+        assert(0 <= b && b < _n);
+        int x = leader(a), y = leader(b);
+        if (x == y) return x;
+        if (-parent_or_size[x] < -parent_or_size[y]) std::swap(x, y);
+        parent_or_size[x] += parent_or_size[y];
+        parent_or_size[y] = x;
+        return x;
+    }
+
+    bool same(int a, int b) {
+        assert(0 <= a && a < _n);
+        assert(0 <= b && b < _n);
+        return leader(a) == leader(b);
+    }
+
+    int leader(int a) {
+        assert(0 <= a && a < _n);
+        if (parent_or_size[a] < 0) return a;
+        return parent_or_size[a] = leader(parent_or_size[a]);
+    }
+
+    int size(int a) {
+        assert(0 <= a && a < _n);
+        return -parent_or_size[leader(a)];
+    }
+
+    std::vector<std::vector<int>> groups() {
+        std::vector<int> leader_buf(_n), group_size(_n);
+        for (int i = 0; i < _n; i++) {
+            leader_buf[i] = leader(i);
+            group_size[leader_buf[i]]++;
+        }
+        std::vector<std::vector<int>> result(_n);
+        for (int i = 0; i < _n; i++) {
+            result[i].reserve(group_size[i]);
+        }
+        for (int i = 0; i < _n; i++) {
+            result[leader_buf[i]].push_back(i);
+        }
+        result.erase(
+            std::remove_if(result.begin(), result.end(),
+                           [&](const std::vector<int>& v) { return v.empty(); }),
+            result.end());
+        return result;
+    }
+
+  private:
+    int _n;
+    // root node: -1 * component size
+    // otherwise: parent
+    std::vector<int> parent_or_size;
+};
+
+}  // namespace atcoder
+
+typedef long long ll;
+#define REP(i, n) for(int i = 0; i < n; i++)
+#define REP2(i, a, b) for(int i = a;i <= b;i++)
+#define REPR(i, a, b) for(int i = a; i >= b; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define INF 2e9
+#define MOD 1000000007
+#define PI 3.14159265358979323846
+#define vi vector<int>
+#define vll vector<ll>
+#define vi2 vector<vector<int>>
+#define eb emplace_back
+#define fi first
+#define se second
+#define ALL(v) v.begin(), v.end()
+#define sz(x) int(x.size())
+using namespace std;
+using namespace atcoder;
+using P = pair<ll,ll>;
+const int dx[]{0, 1, 0, -1, -1, -1, 1, 1}, dy[]{1, 0, -1, 0, -1, 1, -1, 1};
+#define INT(name) int name;cin >> name;
+#define VEC(type,name,n) vector<type> name(n);REP(i,n) cin >> name[i];
+
+template<class T> inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+int main()
+{
+  ll N,M;
+  cin >> N >> M;
+  dsu d(N+M);
+  REP(i,N){
+    ll K;
+    cin >> K;
+    REP(j,K){
+      ll x;
+      cin >> x;
+      d.merge(i+M,x-1);
+    }
+  }
+
+  int flag=0;
+  REP(i,N){
+    if(!d.same(M,i+M)){
+      flag=1;
+    }
+  }
+
+  if(flag==0){
+    cout << "YES" << endl;
+  }else{
+    cout << "NO" << endl;
+  }
+}
