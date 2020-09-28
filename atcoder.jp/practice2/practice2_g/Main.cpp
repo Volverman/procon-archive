3f91e39cf9e82ac2b3b3681c387dc44e460fbf62
Volverman <volverman1017@gmail.com>
2020-09-29 00:33:53 +0900

    [AC] practice2 practice2_g

---
+++ atcoder.jp/practice2/practice2_g/Main.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+typedef long long ll;
+#define REP(i, n) for(int i = 0; i < n; i++)
+#define REP2(i, a, b) for(int i = a;i <= b;i++)
+#define REPR(i, a, b) for(int i = a; i >= b; i--)
+#define FOR(i, m, n) for(int i = m; i < n; i++)
+#define INF 2e9
+#define MOD 1000000007
+#define PI 3.14159265358979323846
+#define vi vector<int>
+#define vll vector<ll>
+#define vi2 vector<vector<int>>
+#define eb emplace_back
+#define fi first
+#define se second
+#define ALL(v) v.begin(), v.end()
+#define sz(x) int(x.size())
+using namespace std;
+using namespace atcoder;
+using P = pair<ll,ll>;
+const int dx[]{0, 1, 0, -1, -1, -1, 1, 1}, dy[]{1, 0, -1, 0, -1, 1, -1, 1};
+#define INT(name) int name;cin >> name;
+#define VEC(type,name,n) vector<type> name(n);REP(i,n) cin >> name[i];
+
+template<class T> inline bool chmin(T& a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+int main()
+{
+  int N,M;
+  cin >> N >> M;
+  vector<P> A(M);
+  scc_graph gr(N);
+  REP(i,M){
+    cin >> A[i].first >> A[i].second;
+    gr.add_edge(A[i].fi,A[i].se);
+  }
+  vector<vector<int>> ans=gr.scc();
+  cout << sz(ans) << endl;
+  REP(i,sz(ans)){
+    cout << sz(ans[i]);
+    for(auto x:ans[i]){
+      cout << " " << x;
+    }
+    cout << endl;
+  }
+}
